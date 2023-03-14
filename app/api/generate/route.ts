64df428bf54c43dd55c3e32fa3371a6d254129ab
Cody De Arkland <codydearkland@gmail.com>
2023-03-14 16:53:08 -0700

    fixing prompt code

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -9,18 +9,20 @@ export const config = {
   };
 
   export async function POST(req: Request): Promise<Response> {
-    console.log(req)
+    
 
-    const body = await req.json()
-    console.log(body)
+    const { prompt } = (await req.json()) as {
+        prompt?: string;
+    };
+    
 
-    if (!body) {
+    if (!prompt) {
         return new Response("No prompt in the request", { status: 400 });
-    }
+    };
 
     const payload: OpenAIStreamPayload = {
         model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: body }],
+        messages: [{ role: "user", content: prompt }],
         temperature: 0.7,
         top_p: 1,
         frequency_penalty: 0,
